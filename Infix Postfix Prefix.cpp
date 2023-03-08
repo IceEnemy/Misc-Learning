fea454080075fd5340d0ce63a86340ba9747ac45
IceEnemy <115701146+IceEnemy@users.noreply.github.com>
2023-03-08 21:46:41 +0700

    infixxxxx

--- Infix Postfix Prefix.cpp
+++ Infix Postfix Prefix.cpp
@@ -8,13 +8,27 @@ struct Node{
 	struct Node *next;
 };
 
+struct opNode{
+	char opr;
+	struct opNode *next;
+};
+
 //struct Node *head = NULL;
 //struct Node *tail = NULL;
 struct Node *top = NULL;
 
+struct opNode *opTop = NULL;
+
 struct Node *addNode(int val){
 	struct Node *curr = (struct Node *)malloc(sizeof(struct Node));
 	curr->op = val;
+	curr->next = NULL;
+	return curr;
+}
+
+struct opNode *addNodeOp(char val){
+	struct opNode *curr = (struct opNode *)malloc(sizeof(struct opNode));
+	curr->opr = val;
 	curr->next = NULL;
 	return curr;
 }
@@ -36,6 +50,23 @@ void addStack(int val){
 	
 }
 
+void firstDataOp(char val){
+	struct opNode *curr = addNodeOp(val);
+	opTop = curr;
+}
+
+void addStackOp(char val){
+	if(opTop == NULL){
+		firstDataOp(val);
+	}
+	else{
+		struct opNode *curr = addNodeOp(val);
+		curr->next = opTop;
+		opTop = curr;
+	}
+	
+}
+
 int rmvStack(){
 	if(top == NULL){
 //		printf("top is null\n");
@@ -43,8 +74,8 @@ int rmvStack(){
 	}
 	int num = top->op;
 	if(top->next == NULL){
-		top = NULL;
 		free(top);
+		top = NULL;
 	}
 	else{
 		struct Node *curr = top->next;
@@ -55,8 +86,27 @@ int rmvStack(){
 	return num;
 }
 
+char rmvStackOp(){
+	if(opTop == NULL){
+//		printf("top is null\n");
+		return NULL;
+	}
+	char num = opTop->opr;
+	if(opTop->next == NULL){
+		free(opTop);
+		opTop = NULL;
+	}
+	else{
+		struct opNode *curr = opTop->next;
+		opTop->next = NULL;
+		free(opTop);
+		opTop = curr;
+	}
+	return num;
+}
+
 void rmvAll(){
-	while(top->next != NULL){
+	while(top != NULL){
 		struct Node *curr = top->next;
 		top->next = NULL;
 		free(top);
@@ -64,6 +114,144 @@ void rmvAll(){
 	}
 	top = NULL;
 	free(top);
+}
+
+void rmvAllOp(){
+	while(opTop != NULL){
+		struct opNode *curr = opTop->next;
+		opTop->next = NULL;
+		free(opTop);
+		opTop = curr;	
+	}
+	opTop = NULL;
+	free(opTop);
+}
+
+int priority(char opr){
+	switch(opr){
+		case '+':
+			return 1;
+		case '-':
+			return 1;
+		case '*':
+			return 2;
+		case '/':
+			return 2;
+		case '^':
+			return 3;
+	}
+	return 0;
+}
+
+int calc(int a, int b, char opr){
+	switch(opr){
+		case '+':
+			return a+b;
+		case '-':
+			return a-b;
+		case '*':
+			return a*b;
+		case '/':
+			return a/b;
+		case '^':
+			return int(pow(a,b));
+	}
+}
+
+void infix(char str[]){
+	int len = strlen(str);
+	int check = true;
+//	printf("len: %d\n",len);
+	int bracket = 0;
+	for(int i=0;i<len;i++){
+//		printf("i = %d\n",i);
+//		printf("str[i] = \'%c\'\n",str[i]);
+		if(str[i] == ' ') continue;
+		else if(str[i] >= '0' && str[i] <= '9'){
+			check = false;
+			int mult = 10;
+			int val = int(str[i++] - '0');
+			while(i != len && str[i] >= '0' && str[i] <= '9'){
+//				printf("val: %d\n",val);
+				val *= mult;
+				val+= (int(str[i++]) - '0');
+			}
+			i--;
+//			printf("val: %d\n",val);
+			addStack(val);
+		}
+		else if(str[i] == '('){
+			check = false;
+			bracket++;
+			addStackOp(str[i]);
+		} 
+		else if(str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '^'){
+			if(check == true){
+				printf("Invalid Notation!\n");
+				return;
+			}
+			check = true;
+			if(opTop == NULL || opTop->next == NULL || priority(str[i]) >= priority(opTop->opr)){
+//				printf("Opr : %c\n", str[i]);
+				addStackOp(str[i]);
+			}
+			else{
+				while(opTop != NULL && opTop->opr != '('){
+					int temp = rmvStack();
+					int temp2 = rmvStack();
+					if(temp2 == NULL){
+						printf("Invalid Notation!\n");
+						return;
+					}
+					char oprtr = rmvStackOp();
+					addStack(calc(temp2,temp,oprtr));
+				}
+			}
+		}
+		else if(str[i] == ')'){
+			check = false;
+			if(bracket < 1){
+				printf("Invalid Notation!\n");
+				return;
+			}
+			else{
+				bracket--;
+				while(opTop->opr != '('){
+					int temp = rmvStack();
+					int temp2 = rmvStack();
+					if(temp2 == NULL){
+						printf("Invalid Notation!\n");
+						return;
+					}
+					char oprtr = rmvStackOp();
+					addStack(calc(temp2,temp,oprtr));
+				}
+				rmvStackOp();
+			}
+		}
+		else{
+			printf("Invalid Notation!\n");
+			return;
+		}
+		
+	}
+	while(opTop != NULL){	
+				int temp = rmvStack();
+				int temp2 = rmvStack();
+				if(temp2 == NULL){
+					printf("Invalid Notation!\n");
+					return;
+				}
+				char oprtr = rmvStackOp();
+				addStack(calc(temp2,temp,oprtr));
+		}
+	if(top->next == NULL){
+		printf("Result : %d\n",top->op);
+	}
+	else{
+		printf("Invalid Notation!\n");
+		return;
+	}
 }
 
 void postfix(char str[]){
@@ -136,8 +324,8 @@ void postfix(char str[]){
 	}
 	else{
 		printf("Invalid Notation!\n");
+		return;
 	}
-	rmvAll();
 }
 
 void prefix(char str[]){
@@ -213,14 +401,6 @@ void prefix(char str[]){
 	rmvAll();
 }
 
-void par(char str[], int idx){
-	
-}
-
-void infix(char str[]){
-	
-}
-
 int main()
 {
 	printf("=============================\n");
@@ -244,18 +424,22 @@ int main()
 				scanf("%[^\n]",operation);
 				getchar();
 				prefix(operation);
+				rmvAll();
 				break;
 			case 2:
 				printf("Enter The Postfix Notation:\n");
 				scanf("%[^\n]",operation);
 				getchar();
 				postfix(operation);
+				rmvAll();
 				break;
 			case 3:
 				printf("Enter The Infix Notation:\n");
 				scanf("%[^\n]",operation);
 				getchar();
-//				infix(operation);
+				infix(operation);
+				rmvAll();
+				rmvAllOp();
 				break;
 			case 4:
 				printf("Program Finished\n");
